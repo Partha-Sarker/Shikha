5aaa568f138957c84e036dca85744a986568eac5
Partha Sarker <parthasarker3@gmail.com>
2020-01-17 04:59:18 +0600

    Dog with animation added

---
+++ Assets/Scripts/StoryManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StoryManager : MonoBehaviour
+{
+    public int nextSceneIndex = 1;
+
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
